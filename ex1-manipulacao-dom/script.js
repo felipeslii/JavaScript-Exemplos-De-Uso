f820367fc436d6211876d321d8d8c38bac07e802
Felipeslii <felipe.l.marcal@aluno.senai.br>
2025-02-24 16:39:38 -0300

    exemplos(completo)

--- ex1-manipulacao-dom/script.js
+++ ex1-manipulacao-dom/script.js
@@ -1,8 +1,14 @@
 // Seleciona o elemento do botão no DOM usando o ID "myButton" e armazena na variável 'button'
 
-// 3ª Digitação (Aqui) 
+const button = document.querySelector('#myButton');
 
 // Seleciona o elemento de parágrafo no DOM usando o ID "myText" e armazena na variável 'text'
 
+const text = document.querySelector('#MyText')
 
 // Adiciona um "ouvinte" de evento ao botão. Quando o botão for clicado, a função dentro do 'addEventListener' será executada
+
+button.addEventListener('click', () => {
+    text.textContent = 'OLá Mundo!!';
+
+});
